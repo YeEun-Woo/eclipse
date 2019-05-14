f51a6aaa6cc67d8645ad0c5f4df5acedfacdf894
예은 우 <co1869@cu.ac.kr>
2019-05-14 15:40:32 +0900

    ghkrdlsanswp

---
+++ eclipse/src/eclipse/Child1.java
@@ -0,0 +1,11 @@
+package eclipse;
+
+public class Child1 extends Parent1 {
+private int studentNo;
+	
+public Child1(String name, int studentNo) {
+		super(name);
+		this.studentNo = studentNo;
+	}
+
+}
