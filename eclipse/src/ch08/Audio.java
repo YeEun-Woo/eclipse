2baa47be6919acde7bd45afd754b5b9291e6be06
예은 우 <co1869@cu.ac.kr>
2019-05-21 16:57:59 +0900

    p.355

--- eclipse/src/ch08/Audio.java
+++ eclipse/src/ch08/Audio.java
@@ -23,6 +23,15 @@ public class Audio implements RemoteControl{
 		}
 		System.out.println("현재 TV볼륨: "+ this.volume);
 	}
+	@Override
+	public void setMute(boolean mute){
+		if(mute) {
+			System.out.println("Audio 무음 처리합니다.");
+		}else {
+			System.out.println("Audio 무음 해제합니다");
+		}
 	}
+}
+
 	
 
