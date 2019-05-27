bf5e4aa3cf033da4a9d23583a726a80f7621baf4
예은 우 <co1869@cu.ac.kr>
2019-05-27 16:14:59 +0900

    확인문제 8-3

---
+++ eclipse/src/ch08/SoundExample.java
@@ -0,0 +1,30 @@
+package ch08;
+
+class Cat implements Soundable{
+	@Override
+	public String sound() {
+		return "具克";
+	}
+}
+
+
+class Dog implements Soundable{
+	@Override
+	public String sound() {
+		return "港港";
+	}
+}
+
+public class SoundExample {
+
+	private static void printSound(Soundable soundable) {
+		System.out.println(soundable.sound());
+	}
+	
+	public static void main(String[] args) {
+	printSound(new Cat());
+	printSound(new Dog());
+
+	}
+
+}
