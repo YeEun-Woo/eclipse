663bda828aca62ba55bbcedad3f5cccdbd37b683
예은 우 <co1869@cu.ac.kr>
2019-05-28 16:22:42 +0900

    p402-403

---
+++ eclipse/src/ch09/Button.java
@@ -0,0 +1,14 @@
+package ch09;
+
+public class Button {
+OnClickListener listener;
+	void setOnClickListener(OnClickListener listener){
+		this.listener = listener;
+	}
+void touch() {
+	listener.onClick();
+}
+interface OnClickListener{
+	void onClick();
+}
+}
